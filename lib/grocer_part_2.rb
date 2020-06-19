dcac7ad67c3ed06f233215933a9738cbf10c3202
Cathy D'Onofrio <catd825@gmail.com>
2020-06-19 20:03:11 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,16 +1,55 @@
 require_relative './part_1_solution.rb'
+require 'pry'
 
 def apply_coupons(cart, coupons)
+  coupons.each_with_index do |hash, index| #iterate over coupon hash and identify its index in array of hashes
+  
+  cart_item = find_item_by_name_in_collection(coupons[index][:item],cart)
+  #create variable that passes through the item and the cart to return the hash of the item passed through
+  
+  couponed_item_by_name = "#{coupons[index][:item]} W/COUPON" #create variable to interpolate current item in interation and 'w/coupon'
+  
+  cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_by_name,cart) #create variable that takes in the couponed item 'w/coupon' (and returns the hash it exists within )
+  
+    if cart_item && cart_item[:count] >= coupons[index][:num] #if the returned cart_item hash's count is greater than the number required to qualify for coupon
+    
+      if cart_item_with_coupon #if the couponed item exists/qualifies for coupon 
+        cart_item_with_coupon[:count] += coupons[index][:num] #return the count of the item that qualifies for coupon and 
+        cart_item[:count] -= coupons[index][:num]
+      else
+        cart_item_with_coupon ={
+          :item =>couponed_item_by_name,
+          :price => coupons[index][:cost]/coupons[index][:num],
+          :clearance => cart_item[:clearance],
+          :count => coupons[index][:num]
+        }
+        cart << cart_item_with_coupon
+        cart_item[:count] -= coupons[index][:num]
+      end
+    end
+   index += 1
+  end
+   cart 
+  end
+  
+
+
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-end
+
+
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
+  binding.pry
+  
+  
+end
+
+# Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-end
+
 
 def checkout(cart, coupons)
   # Consult README for inputs and outputs
