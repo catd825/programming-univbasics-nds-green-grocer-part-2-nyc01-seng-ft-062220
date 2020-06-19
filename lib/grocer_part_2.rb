76d1d234bbe6d6f4b16d737264275057ac872a50
Cathy D'Onofrio <catd825@gmail.com>
2020-06-19 20:41:27 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -60,7 +60,10 @@ def checkout(cart, coupons)
 consolidated_cart = consolidate_cart(cart)
 couponed_cart = apply_coupons(consolidated_cart,coupons)
 final_cart = apply_clearance(couponed_cart)
+total = 0
 
-
+  if each_with_index do |item, index|
+    
+  end
 
 end
